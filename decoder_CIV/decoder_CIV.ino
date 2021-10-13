811746b4a03a67550736a479a916c1e1daaa41b9
unknown <argulab@gmail.com>
2021-10-13 16:20:22 +0200

    Correct bug

---
+++ decoder_CIV/decoder_CIV.ino
@@ -0,0 +1,128 @@
+#include <RTClib.h>
+#include <LiquidCrystal_I2C.h>
+#include "switch.hpp"
+
+uint8_t PreviousMhz = 0;
+uint16_t PreviousKhz = 0;
+uint8_t PreviousActiveBand = 0;
+uint8_t PreviousActiveAntenna = 0;
+
+uint8_t MaxCol = 19;
+uint8_t FrequencyRow = 2;
+uint8_t AntennaRow = 3;
+
+RTC_DS3231 rtc;
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
+//---------------------------------- frequency service
+void init_display () {
+  lcd.setCursor (0, FrequencyRow);
+  lcd.print ("  .   ,    Band=");
+  lcd.setCursor (0, AntennaRow);
+  lcd.print ("Antenna=");
+}
+
+ void display_freq_mhz (uint16_t mhz) {
+  uint8_t mhzCol = 0;
+  if (mhz < 10)
+    mhzCol = 1;
+  lcd.setCursor (0, FrequencyRow);
+  lcd.print ("  ");
+  lcd.setCursor (mhzCol, FrequencyRow);
+  lcd.print (mhz);
+ }
+
+ void display_freq_khz (uint16_t khz) {
+  uint8_t khzCol = 3;
+  lcd.setCursor (khzCol, FrequencyRow);
+  if (khz < 100 && khz >= 10)
+    lcd.print ("0");
+  if (khz < 10)
+    lcd.print ("0");
+  lcd.print (khz);
+ }
+
+ void display_freq_hz (uint16_t hz) {
+  uint8_t hzCol = 7;
+  lcd.setCursor (hzCol, FrequencyRow);
+  if (hz < 10)
+    lcd.print ("0");
+  lcd.print (hz);
+ }
+
+ void display_band (String band) {
+  uint8_t bandPos = 16;
+  lcd.setCursor (bandPos, FrequencyRow);
+  lcd.print ("    ");
+  lcd.setCursor (bandPos, FrequencyRow);
+  lcd.print (band);
+ }
+
+void display_active_antenna (uint8_t ant) {
+  uint8_t antennaPos = 9;
+  lcd.setCursor (antennaPos, AntennaRow);
+  lcd.print ("         ");
+  lcd.setCursor (antennaPos, AntennaRow);
+  lcd.print (name_of_antenna (ant));
+ }
+
+void frequency_service (uint32_t frq) {
+  uint16_t frqMhz = int (frq / 100000);
+  uint16_t frqKhz = int (frq / 100) - (frqMhz * 1000);
+  uint16_t frqHz = frq % 100;
+
+  if (frqMhz != PreviousMhz) {
+    PreviousMhz = frqMhz;
+    display_freq_mhz (frqMhz);
+  }
+  if (frqKhz != PreviousKhz) {
+    PreviousKhz = frqKhz;
+    display_freq_khz (frqKhz);
+  }
+  display_freq_hz (frqHz);
+
+  uint8_t activeBand = decode_band (frq);
+  if (activeBand != PreviousActiveBand) {
+    PreviousActiveBand = activeBand;
+    display_band (name_of_band (activeBand));
+
+    uint8_t activeAntenna = select_antenna (activeBand);
+    if (activeAntenna != PreviousActiveAntenna) {
+      PreviousActiveAntenna = activeAntenna;
+      display_active_antenna (activeAntenna);
+      service_switch_antenna (activeAntenna);
+    }
+  } 
+}
+
+void setup () {
+  const unsigned long serial0Rate = 115200;
+  const uint16_t serial1Rate = 19200;
+    
+  Serial.begin (serial0Rate);
+  Serial.flush ();
+  Serial1.begin (serial1Rate);
+  Serial1.flush ();
+
+  lcd.init ();
+  rtc.begin (); 
+  lcd.backlight();
+  lcd.clear();
+
+  init_relays_pins ();
+  init_display ();
+  request_frequency ();
+  
+  DateTime dtNow = rtc.now ();
+//  set_date (dtNow);
+//  set_time (dtNow);
+}
+
+void loop () {  
+  if (Serial1.available ()) {
+    uint32_t frequency = process_frequency (); 
+    if (frequency != 0) {
+      frequency_service (frequency);
+    }
+  }
+}
---
+++ decoder_CIV/switch.hpp
@@ -0,0 +1,281 @@
+//#define DEBUG_FRAME
+#define DEBUG_RELAYS_STATES
+
+//--------------------------- byte frame value
+const uint8_t SOFrame = 0xFE;
+const uint8_t EOFrame = 0xFD;
+const uint8_t COMrequest = 0x03;
+const uint8_t COMtransceive = 0x00;
+const uint8_t RIGaddress = 0x94;
+const uint8_t CONTROLaddress = 0x0E;
+
+/*                Byte position in the frame
+ * -------------------------------------------------------------
+ * |0xFE|0xFE|0x0E|0x94|0x03/0x00|BCD1|BCD2|BCD3|BCD4|BCD5|0xFD|
+ * -------------------------------------------------------------
+ * | 0  | 1  | 2  | 3  |    4    | 5  | 6  | 7  | 8  | 9  | 10 |
+ * -------------------------------------------------------------
+ * |Prelude  | To |From|Command  |          Data          |End |
+ * ------------------------------------------------------------
+ * When the frequency data comming by a request, the command is 0x03
+ * When the frequency data comming by a change in the rig, (transceive 'ON' mode) the command is 0x00
+ */
+
+const uint8_t SOFpos = 0;
+const uint8_t COMMANDpos = 4;
+const uint8_t DATA1pos = 5;
+const uint8_t DATA2pos = 6;
+const uint8_t DATA3pos = 7;
+const uint8_t DATA4pos = 8;
+const uint8_t DATA5pos = 9;
+const uint8_t EOFpos = 10;
+
+const uint8_t RELAY1pin = 32;
+const uint8_t RELAY2pin = 34;
+const uint8_t RELAY3pin = 36;
+const uint8_t RELAY4pin = 38;
+const uint8_t RELAY5pin = 40;
+const uint8_t RELAY6pin = 42;
+const uint8_t RELAY7pin = 44;
+const uint8_t RELAY8pin = 46;
+
+uint32_t PreviousKhertz = 0;
+uint8_t PreviousHertz = 0;
+uint8_t FrequencyFrame [11];
+uint8_t FramePos = 0;
+uint8_t MaxRelay = 7;
+
+//-------------------------- enum variables
+enum bands {
+  b0, b6, b10, b12, b15, b17, b20, b30, b40, b60, b80, b160
+  };
+enum antennas {
+  none, hexbeam, aperiodic, ant3, ant4
+  };
+char RelayPins [] = {32, 34, 36, 38, 40, 42, 44, 46};
+
+//------------------------------------- DEBUG
+void debug_relay_states () {
+  for (uint8_t i = 0; i <= MaxRelay; i++) {
+    uint8_t pinRelay = RelayPins [i];
+    Serial.print ("Rele num = ");
+    Serial.print (pinRelay);
+    Serial.print (" Estado = ");
+    Serial.println (digitalRead (pinRelay));
+  }
+}
+
+//------------------------------------- decoders
+uint8_t hex_to_dec (uint8_t hexValue) {
+  return ((hexValue >> 4) * 10) + (hexValue & 0x0F);
+}
+
+uint8_t dec_to_hex (uint8_t value) {
+  return ((value/10) *16) + (value % 10);
+}
+
+void init_relays_pins () {
+  for (uint8_t relayNum = 0; relayNum <= MaxRelay; relayNum++) {
+    uint8_t relayPin = RelayPins [relayNum];
+    pinMode (relayPin, OUTPUT);
+    digitalWrite (relayPin, LOW);
+  }
+}
+
+//------------------------------------ REQUEST 
+void request_frequency () {
+  Serial1.flush();
+  Serial1.write(SOFrame); 
+  Serial1.write(SOFrame); 
+  Serial1.write(RIGaddress); 
+  Serial1.write(CONTROLaddress);
+  Serial1.write(COMrequest); 
+  Serial1.write(EOFrame); 
+  delay(20); 
+}
+
+void debug_frame (uint16_t pos, uint16_t vHex, uint16_t vDec) {
+  Serial.print ("Pos = ");
+  Serial.print (pos);
+  Serial.print (" ValHex = ");
+  Serial.print (vHex, HEX);
+  Serial.print (" ValDec = ");
+  Serial.println (vDec, DEC);
+}
+
+//-------------------------- set Date and Time
+void set_date (DateTime dtn) {
+  uint8_t now_year = dtn.year();
+  uint8_t now_month = dtn.month();
+  uint8_t now_day = dtn.day();
+ 
+  Serial1.flush();
+  Serial1.write(SOFrame); 
+  Serial1.write(SOFrame); 
+  Serial1.write(RIGaddress); 
+  Serial1.write(CONTROLaddress);
+  Serial1.write(0x1A); 
+  Serial1.write(0x05);
+  Serial1.write(0x00);
+  Serial1.write(0x94);
+  Serial1.write(dec_to_hex(int(now_year/100)));
+  Serial1.write(dec_to_hex(now_year % 100));
+  Serial1.write(dec_to_hex(now_month));
+  Serial1.write(dec_to_hex(now_day));
+  Serial1.write(EOFrame); 
+  delay(20); 
+}
+
+void set_time (DateTime dtn) {
+  int now_hour = dtn.hour ();
+  int now_minute = dtn.minute ();
+ 
+  Serial1.flush();
+  Serial1.write(SOFrame); 
+  Serial1.write(SOFrame); 
+  Serial1.write(RIGaddress); 
+  Serial1.write(CONTROLaddress);
+  Serial1.write(0x1A); 
+  Serial1.write(0x05);
+  Serial1.write(0x00);
+  Serial1.write(0x95);
+  Serial1.write(dec_to_hex(now_hour));
+  Serial1.write(dec_to_hex(now_minute));
+  Serial1.write(EOFrame); 
+  delay(20); 
+}
+
+//-------------------------------------------- DECODER FUNCTIONS
+antennas select_antenna (bands band) {
+  switch (band) {
+    case b6: return ;
+    case b10: return hexbeam;
+    case b12: return hexbeam;
+    case b15: return hexbeam;
+    case b17: return hexbeam;
+    case b20: return hexbeam;
+    case b30: return aperiodic;
+    case b40: return aperiodic;
+    case b60: return aperiodic;
+    case b80: return ant3;
+    case b160: return ant4;
+    default: return none;
+  }
+}
+
+String name_of_antenna (antennas ant) {
+  switch (ant) {
+    case none:      return "NONE";
+    case hexbeam:   return "HEXBEAM";
+    case aperiodic: return "APERIODIC";
+    case ant3:      return "Antenna 3";
+    case ant4:      return "Antenna 4";
+  }
+}
+
+bands decode_band (uint32_t frq) {
+  if (frq >=  181000 && frq <=  200000) return b160;
+  if (frq >=  350000 && frq <=  380000) return b80;
+  if (frq >=  535100 && frq <=  536600) return b60;
+  if (frq >=  700000 && frq <=  720000) return b40;
+  if (frq >= 1010000 && frq <= 1015000) return b30;
+  if (frq >= 1400000 && frq <= 1435000) return b20;
+  if (frq >= 1806800 && frq <= 1816800) return b17;
+  if (frq >= 2100000 && frq <= 2145000) return b15;
+  if (frq >= 2489000 && frq <= 2499000) return b12;
+  if (frq >= 2800000 && frq <= 2970000) return b10;
+  if (frq >= 5000000 && frq <= 5200000) return b6;
+
+  return b0;
+}
+
+String name_of_band (bands b) {
+  switch (b) {
+    case b0:   return "NULL";
+    case b160: return "160m";
+    case b80:  return "80m";
+    case b60:  return "60m";
+    case b40:  return "40m";
+    case b30:  return "30m";
+    case b20:  return "20m";
+    case b17:  return "17m";
+    case b15:  return "15m";
+    case b12:  return "12m";
+    case b10:  return "10m";
+    case b6:   return "6m";
+  }
+}
+
+uint8_t select_relay_antenna (antennas ant) {
+  switch (ant) {
+    case hexbeam:   return 0;
+    case aperiodic: return 1;
+    case ant3:      return 2;
+    case ant4:      return 3;
+  }
+}
+
+uint32_t decode_frequency () {
+  uint32_t frq = 0;
+
+  if (FrequencyFrame [SOFpos] != SOFrame)
+    return 0;
+    
+  if (FrequencyFrame [COMMANDpos] != COMrequest && FrequencyFrame [COMMANDpos] != COMtransceive)
+    return 0;
+    
+  if (FrequencyFrame [EOFpos] != EOFrame)
+    return 0;
+
+  for (uint8_t bytePos = SOFpos; bytePos <= EOFpos; bytePos++) {
+    uint32_t hexValue = FrequencyFrame [bytePos];
+    uint32_t decValue = hex_to_dec (hexValue);
+
+    #ifdef DEBUG_FRAME
+      debug_frame (bytePos, hexValue, decValue);
+    #endif  //DEBUG_FRAME    
+
+    switch (bytePos) {
+      case DATA1pos:
+        frq += int (decValue / 10); break;
+      case DATA2pos:
+        frq += decValue * 10; break;
+      case DATA3pos:
+        frq += decValue * 1000; break;
+      case DATA4pos:
+        frq += decValue * 100000; break;
+      case DATA5pos:
+        frq += decValue * 10000000; break;
+    }
+  }
+  return frq; 
+}
+
+uint32_t process_frequency () {
+  while (Serial1.available ()) {
+    uint8_t incomingByte = Serial1.read ();
+    FrequencyFrame [FramePos] = incomingByte;
+    FramePos++;
+    if (incomingByte == EOFrame){
+      FramePos = 0;
+      return decode_frequency ();      
+    }
+  }
+}
+
+void service_switch_antenna (antennas antennaService) {
+  uint8_t activeRelay = select_relay_antenna (antennaService);
+  
+  
+  for (uint8_t relayNum = 0; relayNum <= MaxRelay; relayNum++) {
+    uint8_t pinRelay = RelayPins [relayNum];
+    if (relayNum == activeRelay) {
+      digitalWrite (pinRelay, HIGH);
+    } else {
+      digitalWrite (pinRelay, LOW);
+    }
+  }
+  #ifdef DEBUG_RELAYS_STATES
+    debug_relay_states ();
+  #endif  //DEBUG_RELAYS_STATES
+}
